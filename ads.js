18858a90f22bd32a0abe5b6e856385df698a5e3b
Ali Damaj <90399598+Alidamaj@users.noreply.github.com>
2023-08-07 00:22:05 +0300

    ads.js

---
+++ ads.js
@@ -0,0 +1,9 @@
+
+	atOptions = {
+		'key' : '8a41ac7ddd1de19d9b55401f363f66c3',
+		'format' : 'iframe',
+		'height' : 50,
+		'width' : 320,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.profitablecreativeformat.com/8a41ac7ddd1de19d9b55401f363f66c3/invoke.js"></scr' + 'ipt>');
